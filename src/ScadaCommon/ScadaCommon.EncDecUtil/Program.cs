5c836cae8eff3974f891888854e149f8c1b55412
vponomarev <fortnum@mail.ru>
2023-10-17 20:58:07 +0300

    .

--- src/ScadaCommon/ScadaCommon.EncDecUtil/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Scada;
-
-if (args.Length == 0) return;
-
-var str = ScadaUtils.Encrypt(args[0]);
-
-Console.WriteLine(str);
---
+++ src/Serven/ViewExplorerPlugin/ViewExplorerPluginLogic.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.DependencyInjection;
+using Scada.Web.Plugins;
+using Scada.Web.Services;
+using System.Diagnostics.Metrics;
+using System.Text;
+
+namespace Scada.Web.Plugins.ServenViewExplorerPlugin;
+
+public class ServenViewExplorerPluginLogic : PluginLogic, IPageFilter, IResultFilter, IResourceFilter
+{
+    public ServenViewExplorerPluginLogic(IWebContext webContext) : base(webContext)
+    {
+    }
+
+    public override void AddFilters(FilterCollection filters)
+    {
+        base.AddFilters(filters);
+
+        filters.Add<ServenViewExplorerPluginLogic>();
+    }
+
+    public override void AddServices(IServiceCollection services)
+    {
+        base.AddServices(services);
+    }
+
+    public void OnPageHandlerSelected(PageHandlerSelectedContext context)
+    {
+
+    }
+
+    public void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+    {
+    }
+
+    public void OnPageHandlerExecuted(PageHandlerExecutedContext context)
+    {
+
+    }
+
+    Stream _prevStream;
+
+    public void OnResultExecuting(ResultExecutingContext context)
+    {
+        var response = context.HttpContext.Response;
+        _prevStream = response.Body;
+        //response.Body = new MemoryStream();
+    }
+
+    public void OnResultExecuted(ResultExecutedContext context)
+    {
+        var response = context.HttpContext.Response;
+        //response.Body.Position = 0;
+        //var str = new StreamReader(response.Body, Encoding.UTF8).ReadToEnd();
+    }
+
+    public void OnResourceExecuting(ResourceExecutingContext context)
+    {
+    }
+
+    public void OnResourceExecuted(ResourceExecutedContext context)
+    {
+    }
+}
